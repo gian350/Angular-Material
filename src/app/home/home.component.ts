fce320428646b366974b85d7f8678ba8b24c5c0b
gian <gian_ru350@hotmail.com>
2020-10-04 21:41:30 -0500

    Tarea 3: Semana 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Dish } from '../shared/Dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
+import { leader } from '../shared/Leader ';
+import { LeaderService}  from '../services/leader.service';
 
 
 @Component({
@@ -15,13 +17,15 @@ export class HomeComponent implements OnInit {
 
   dish:Dish;
   promo:Promotion;
+  lead:leader;
 
   // se inyecta el serivicio a la clase mediante el constructor
-  constructor( private dishService: DishService, private promotionService: PromotionService) { }
+  constructor( private dishService: DishService, private promotionService: PromotionService, private leaderService: LeaderService) { }
 
   ngOnInit(): void {
     this.dish = this.dishService.getFeaturedDish();
     this.promo = this.promotionService.getFeaturedPromotion();
+    this.lead = this.leaderService.getFeaturedLeader();
   }
 
 }
